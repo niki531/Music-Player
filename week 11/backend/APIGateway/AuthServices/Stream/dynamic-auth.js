850ecfad23e60948f4d4e878edfb2b9b81450b8d
niki531 <137993765+niki531@users.noreply.github.com>
2024-09-07 18:19:18 -0400

    Delete week 11 directory

--- week 11/backend/APIGateway/AuthServices/Stream/dynamic-auth.js
+++
@@ -1,9 +0,0 @@
-export default async function checkAccess(ctx, next) {
-    const authAllowed = (ctx.headers['x-auth-verified'] === 'true') && (ctx.headers['x-user-subscribe'] === 'premium');
-    if (!authAllowed) {
-        ctx.status = 403;
-        ctx.body = { error: 'Access denied' };
-        return;
-    }
-    await next();  
-}
--- week 11/backend/APIGateway/jwtGateway/utils/blacklistUtils.js
+++
@@ -1,8 +0,0 @@
-import Blacklist from '../models/blacklist.js'; 
-
-export const isTokenBlacklisted = async (token) => {
-  const blacklisted = await Blacklist.findOne({ token });
-  const result = blacklisted!== null
-  // console.log('blacklisted?',result);
-  return result ;
-};
